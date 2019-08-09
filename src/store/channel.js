1574f70b92877692fffb3525275076fd1722b44c
yang <exvincse@gmail.com>
2019-08-09 22:24:24 +0800

    修改env

--- src/store/channel.js
+++ src/store/channel.js
@@ -10,8 +10,13 @@ export default {
 			getdata(context, { lange, name, startdata = 0 }) {
 					context.dispatch('Loading', true, { root: true });
 					return new Promise((resolve) => {
-						const api = `${process.env.VUE_APP_APIPATH}/kraken/streams/?client_id=${process.env.VUE_APP_CUSTOMPATH}&language=${lange}&game=${name}&offset=${startdata}`;
-						axios.get(api).then((response) => {
+						const api = `${process.env.VUE_APP_APIPATH}/kraken/streams/?language=${lange}&game=${name}&offset=${startdata}`;
+						axios.get(api, {
+							headers: {
+								'Accept': 'application/vnd.twitchtv.v5+json',
+								'Client-ID':`${process.env.VUE_APP_CUSTOMPATH}`,
+							},
+						}).then((response) => {
 							context.commit('CHANNEL', response);
 							context.dispatch('Loading', false, { root: true });
 							resolve();

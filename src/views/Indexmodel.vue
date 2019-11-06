b5c10259ad2ff3cf4e1c73a3104e18f410d83994
yang <exvincse@gmail.com>
2019-11-06 14:14:15 +0800

    modify css

--- src/views/Indexmodel.vue
+++ src/views/Indexmodel.vue
@@ -1,48 +1,41 @@
 <template>
   <div>
     <div class="bg-cover">
-      <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <router-link class="navbar-brand mr-0 mr-lg-4" to="/">
-          <img class="d-lg-block d-none" src="../assets/img/twitchlogo.png" style="max-width:90px" alt="">
-          <img class="d-lg-none d-block" src="../assets/img/rwdlogo.png" style="max-width:20px" alt="">
+      <nav class="l-nav bg-primary">
+        <router-link class="nav-logo mr-0 mr-lg-4" to="/">
+          <img class="desktop-img" src="../assets/img/twitchlogo.png" style="max-width:90px" alt="">
+          <img class="mobile-img" src="../assets/img/rwdlogo.png" style="max-width:20px" alt="">
         </router-link>
-
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav mr-5">
-            <li class="nav-item mr-3">
+        <div class="menu-list">
+          <ul class="mr-5">
+            <li class="menu-list-item mr-3">
               <router-link class="nav-link l-border"
                       exact-active-class="l-active"
                       to="/">首頁</router-link>
             </li>
-            <li class="nav-item">
+            <li class="menu-list-item">
               <router-link class="nav-link l-border"
                       exact-active-class="l-active"
                       to="/game">遊戲</router-link>
             </li>
           </ul>
-          <div class="input-group w-50">
-            <input type="text" class="form-control rounded-0" placeholder="請輸入實況主名稱"
+          <div class="live-search">
+            <input type="text" class="rounded-0" placeholder="請輸入實況主名稱"
               v-model="name"
               @keyup.enter="liveselect()">
             <button class="btn btn-success rounded-0 input-group-append"
                 @click.prevent="liveselect()">查詢</button>
           </div>
         </div>
-        <div class="input-group w-60 d-lg-none d-flex">
-          <input type="text" class="form-control rounded-0" placeholder="請輸入實況主名稱"
-            v-model="name"
-            @keyup.enter="liveselect()">
-          <button class="btn btn-success rounded-0 input-group-append"
-              @click.prevent="liveselect()">查詢</button>
-        </div>
-        <div class="d-lg-none d-block">
+        <div class="mobile-menu-bar">
           <button class="btn navbar-toggler"
             @click="slider()">
             <i class="fas fa-bars text-white fa-lg"></i>
           </button>
         </div>
-        <div class="navbar-collapse search">
-          <ul class="navbar-nav mr-lg-5 w-100">
+
+        <div class="search">
+          <ul class="mr-lg-5 w-100">
             <li class="nav-item py-1 mr-lg-3"
               @click="DropHide()">
               <router-link class="nav-link l-border"

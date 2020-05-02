bc512497c019ec62835c12234a96f7d2a1917db7
yang <exvincse@gmail.com>
2020-05-02 20:35:26 +0800

    push2

--- src/store/channel.js
+++ src/store/channel.js
@@ -1,32 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  namespaced: true,
-  state: {
-    total: 0,
-    channel: [],
-  },
-  actions: {
-    async getdata(context, { lange, name, startdata = 0 }) {
-      const api = `${process.env.VUE_APP_APIPATH}/kraken/streams/?language=${lange}&game=${name}&offset=${startdata}`;
-      const res = await axios.get(api, {
-        headers: {
-          Accept: 'application/vnd.twitchtv.v5+json',
-          'Client-ID': `${process.env.VUE_APP_CUSTOMPATH}`,
-        },
-      });
-      context.commit('CHANNEL', res);
-    },
-  },
-  mutations: {
-    CHANNEL(state, payload) {
-      state.total = payload.data['_total'];
-      state.channel = payload.data.streams;
-    },
-  },
-  getters: {
-    channeldata(state) {
-      return state.channel;
-    },
-  },
+	namespaced: true,
+	state: {
+		total: 0,
+		channel: []
+	},
+	actions: {
+		async getdata(context, { lange, name, startdata = 0 }) {
+			const api = `${process.env.VUE_APP_APIPATH}/kraken/streams/?broadcaster_language=${lange}&game=${name}&offset=${startdata}`;
+			const res = await axios.get(api, {
+				headers: {
+					Accept: "application/vnd.twitchtv.v5+json",
+					"Client-ID": `${process.env.VUE_APP_CUSTOMPATH}`
+				}
+			});
+			context.commit("CHANNEL", res);
+		}
+	},
+	mutations: {
+		CHANNEL(state, payload) {
+			state.total = payload.data["_total"];
+			state.channel = payload.data.streams;
+		}
+	},
+	getters: {
+		channeldata(state) {
+			return state.channel;
+		}
+	}
 };

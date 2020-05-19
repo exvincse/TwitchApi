c33e92447b7d0bb7082409373535ac7bbfc43ccb
yang <exvincse@gmail.com>
2020-05-19 23:58:09 +0800

    update1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,30 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "@vue/airbnb",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "no-tabs": 0,
-        "max-len": [
-            "error",
-                {
-                "code": 300
-                }
-        ],
-        "dot-notation": 0,
-        "no-param-reassign": 0,
-    }
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: ["eslint:recommended", "@vue/airbnb", "plugin:vue/essential"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: "module"
+	},
+	plugins: ["vue"],
+	rules: {
+		"no-tabs": 0,
+		"max-len": [
+			"error",
+			{
+				code: 300
+			}
+		],
+		indent: ["error", "tab"],
+		"dot-notation": 0,
+		"no-param-reassign": 0
+	}
 };
